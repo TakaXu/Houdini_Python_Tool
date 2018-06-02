b2701126187f0e1a8a13bdba099e635173958133
TakaSama <32671440+TakaXu@users.noreply.github.com>
2018-06-02 19:49:01 +0800

    Update Auto Cache.py

--- Auto Cache.py
+++ Auto Cache.py
@@ -15,15 +15,24 @@ def cache(node):
 
 def rop(node):
     selectNode.parm("execute").pressButton()
-    
+
+def dop(node):
+    selectNode.parm("execute").pressButton()
+    selectNode.parm("loadfromdisk").set(1)
+    selectNode.parm("reload").pressButton()
+    selectNode.setRenderFlag(1)
+    selectNode.setDisplayFlag(1)  
+
 selectNodes = list(hou.selectedNodes())
 
 for selectNode in selectNodes:
     name = selectNode.type().name()
-    print name
+#    print name
     if name == "catche_tool_1.0.1":
         catche(selectNode)
     if name == "filecache":
         cache(selectNode)
     if name == "rop_geometry":
         rop(selectNode)
+    if name == 'dopio':
+        dop(selectNode)
