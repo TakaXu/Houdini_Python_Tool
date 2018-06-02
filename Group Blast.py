c84e684e0de64c9c1c6f187f2942f77e83a0e61a
TakaSama <32671440+TakaXu@users.noreply.github.com>
2018-06-02 19:49:56 +0800

    Create Group Blast.py

---
+++ Group Blast.py
@@ -0,0 +1,22 @@
+selectNodes = list(hou.selectedNodes())
+
+grplist = list()
+grpnames = list()
+
+
+def primGroupNames(geometry):
+    groupnames = geometry.primGroups()
+    for grp in groupnames:
+        grplist.append(grp.name())
+        grp_names = grplist[-1]
+        grpnames.append(grp_names)
+        print grpnames
+    return grpnames
+        
+for selectNode in selectNodes:
+    primGroupNames(selectNode.geometry())
+    for grpname in grpnames:
+        blastNode = selectNode.createOutputNode("blast")
+        blastNode.parm("group").set(grpname)
+        blastNode.parm("negate").set(1)
+    
