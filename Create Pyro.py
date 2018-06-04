ade0eeb0a6890c2f212a9521a48bbb7f6c823104
TakaSama <32671440+TakaXu@users.noreply.github.com>
2018-06-04 20:15:31 +0800

    Create Create Pyro.py

---
+++ Create Pyro.py
@@ -0,0 +1,36 @@
+color1 = hou.Color()
+color1.setRGB((0.9,0,0))
+
+color2 = hou.Color()
+color2.setRGB((0.45,0,0.9))
+
+color3 = hou.Color()
+color3.setRGB((0,0.4,0))
+
+plane = hou.ui.paneTabOfType(hou.paneTabType.NetworkEditor)
+
+pos = plane.selectPosition()
+
+node = plane.currentNode()
+
+try:
+    dopNode = node.createNode("dopnet")
+except:
+    temNode = node.parent()
+    dopNode = temNode.createNode("dopnet")
+   
+dopNode.setPosition(pos)
+dopNode.setColor(color2)
+
+try:
+    dopNode.setName("dopNode_pyro")
+except:
+    pass
+    
+for node in dopNode.children():
+    if node.name() == "output":
+        pyroNode = node.createInputNode(0,"pyrosolver")
+        sobjNode = pyroNode.createInputNode(0,"smokeobject")
+        reSizeNode = pyroNode.createInputNode(1,"gasresizefluiddynamic")
+        svNode = pyroNode.createInputNode(4,"sourcevolume")
+        
